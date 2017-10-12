1950d4f40200e07959b42b3e3bdded12538c7136
pawelkupka <pawel.kupka@gmail.com>
2017-10-12 20:45:48 +0200

    start

---
+++ MongoDBSample/MongoDBSample/Models/CustomerRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace MongoDBSample.Models
+{
+    public class CustomerRepository
+    {
+        private readonly IMongoDatabase _db;
+
+        public CustomerRepository()
+        {
+            var client = new MongoClient();
+            _db = client.GetDatabase("MongoSampleDb");
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            var filter = Builders<Customer>.Filter.Empty;
+            return _db.GetCollection<Customer>("Customers").Find(filter).ToList();
+        }
+
+        public void Delete(int id)
+        {
+            _db.GetCollection<Customer>("Customers").FindOneAndDelete(c => c.Id == id);
+        }
+
+        public void Insert(Customer customer)
+        {
+            _db.GetCollection<Customer>("Customers").InsertOne(customer);
+        }
+
+        public void Update(Customer customer)
+        {
+            var filter = Builders<Customer>.Filter.Eq("Id", customer.Id);
+            var update = Builders<Customer>.Update.Set("Name", customer.Name);
+            _db.GetCollection<Customer>("Customers").UpdateOne(filter, update);
+        }
+
+        public void DeleteAll()
+        {
+            var filter = Builders<Customer>.Filter.Empty;
+            _db.GetCollection<Customer>("Customers").DeleteMany(filter);
+        }
+    }
+}
---
+++ MongoDBSample/MongoDBSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using MongoDBSample.Models;
+
+namespace MongoDBSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerRepository = new CustomerRepository();
+            customerRepository.DeleteAll();
+            customerRepository.Insert(new Customer { Id = 1, Name = "Name1" });
+            customerRepository.Insert(new Customer { Id = 2, Name = "Name2" });
+            customerRepository.Insert(new Customer { Id = 3, Name = "Name3" });
+            customerRepository.Insert(new Customer { Id = 4, Name = "Name4" });
+            customerRepository.Update(new Customer { Id = 2, Name = "Name22" });
+            customerRepository.Delete(3);
+            var allCustomers = customerRepository.GetAll();
+            allCustomers.ToList().ForEach(c => Console.WriteLine(c.Name));
+        }
+    }
+}
